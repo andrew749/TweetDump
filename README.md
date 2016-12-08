fe9393f3b0bcf43f5ac6e89eb0a3aeaea2fdddb3
Andrew Codispoti <andrewcod749@gmail.com>
2016-12-07 23:23:37 -0800

    Added README.

---
+++ README.md
@@ -0,0 +1,20 @@
+# Tweet Dump
+
+A simple script to dump random tweet data from twitter.
+
+I found this as a useful way to get realtime tweets (from a random sample)
+since there doesn't seem to be any good data sets out there. This script will
+dump all the data twitter returns about the tweet.
+
+## Prerequisites
+
+- Folder in the root of this project named `dump`.
+
+## Usage
+
+```
+python dump_data.py
+```
+
+Nothing too crazy.
+
--- dump_data.py
+++ dump_data.py
@@ -3,29 +3,30 @@ from uuid import uuid4
 import json
 import os
 
-api = twitter.Api(consumer_key='xxxxxxxxxxxxxxxxxxxxxxxxx',
-consumer_secret='xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-access_token_key='xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-access_token_secret='xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx')
+# REPLACE KEYS AS NEEDED
+api = twitter.Api(
+    consumer_key='xxxxxxxxxxxxxxxxxxxxxxxxx',
+    consumer_secret='xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    access_token_key='xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    access_token_secret='xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+)
 
 # verify that the credentials still work
 api.VerifyCredentials()
 stream = api.GetStreamSample()
 
 def get_new_tweet_data():
-    print "Getting more data"
-    # get a small batch
+    # get a small batch of tweets to precent going over limits
     tweets = [stream.next() for x in xrange(20)]
-    print "Got data"
     return tweets
 
 # how many tweets to store in a single file
 FILE_SIZE_NUM_TWEETS = 1000
 
 def write_tweets_to_file(tweets, file):
-    print "Writing temp file"
     file.write(json.dumps(tweets))
 
+# Continually dump.
 while True:
     # keep dumping data to disk
     current_file_size = 0
